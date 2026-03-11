3425616241c5c2180abb25f1a138bf2973f6fd4c
Surendra26 <yanamadalasurendra@gmai.com>
2026-03-11 22:17:14 +0530

    Fix API URL

--- backend/slotbooking/settings.py
+++ backend/slotbooking/settings.py
@@ -134,6 +134,11 @@ CORS_ALLOWED_ORIGINS = [
 
 ]
 CORS_ALLOW_ALL_ORIGINS = True  
+CORS_ALLOWED_ORIGINS = [
+    "https://slot-booking-project.vercel.app",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
