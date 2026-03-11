d2ba74326b6142b1cbbbc04e8b0a7f75aff065d7
Surendra26 <yanamadalasurendra@gmai.com>
2026-03-11 16:21:58 +0530

    Update settings for production

--- backend/slotbooking/settings.py
+++ backend/slotbooking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&wsyt%ir-200xvnc43%n4nj-m_pjk3b+flye_2%h4pofqe-a_9'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&wsyt%ir-200xvnc43%n4nj-m_pjk3b+flye_2%h4pofqe-a_9')
+DEBUG = False
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
