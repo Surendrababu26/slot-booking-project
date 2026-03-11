6d2ea515be1986041faec58c3387d00de49b6156
Surendra26 <yanamadalasurendra@gmai.com>
2026-03-11 22:08:33 +0530

    Fix API URL

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,5 @@
 // API Base URL
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://slot-booking-project-tpe9.onrender.com/api';
 
 // Format time slot for display
 function formatTimeSlot(timeSlot) {
